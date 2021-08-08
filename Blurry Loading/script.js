0ca04059a838a2fe72d92cf414de2886fd228f04
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2021-08-08 16:11:28 +0200

    Blurry Loading

---
+++ Blurry Loading/script.js
@@ -0,0 +1,28 @@
+const loadingText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {
+
+  load++;
+  
+  loadingText.textContent = `${load}%`;
+
+  loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+
+  if (load == 100) {
+    clearInterval(int);
+  }
+
+}
+
+/***************************************************************************/
+/*general case for scaling  number between two numbers to defrent two numbers*/
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
